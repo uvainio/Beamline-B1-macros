f9a7abc5c31dc547a567d9df379e8c44669330e6
Ulla Vainio <ulla.vainio@gmail.com>
2009-10-29 14:58:04 +0100

    Improvements to reintegrate by AW and UV (removed first q-value, because its intensity is not real)

--- macrospilatus/reintegrateB1pilatus.m
+++ macrospilatus/reintegrateB1pilatus.m
@@ -1,5 +1,5 @@
-function [qs,ints,errs,areas,params]=reintegrateB1pilatus(fsn,mask,sddistance,qrange)
-% function [qs,ints,errs,areas,params]=reintegrateB1pilatus(fsn,mask,sddistance,qrange)
+function reintegrateB1pilatus(fsn,mask,sddistance,qrange,samplenames)
+% function reintegrateB1pilatus(fsn,mask,sddistance,qrange,samplenames)
 %
 % Re-integrate 2d scattering patterns
 %
@@ -7,20 +7,30 @@ function [qs,ints,errs,areas,params]=reintegrateB1pilatus(fsn,mask,sddistance,qr
 %        fsn: file sequence numbers (a matrix can be also supplied).
 %           The files int2dnorm*.dat and err2dnorm*.dat, as well as
 %           intnorm*.log should exist and be accessible.
-%        mask: mask to be used for the integration
+%        mask: mask to be used for the integration. 0 means masked, nonzero
+%           means nonmasked.
+%        sddistance: sample-to-detector distance. Only measurements with
+%           this distance are re-integrated. Giving more than one is
+%           possible. Give empty to re-integrate measurements with every
+%           distance.
 %        qrange: a range of q values, onto which the integrated data should
 %           fall. If omitted, a common q-range is created by taking the mask
 %           into account. If a scalar is supplied, it is regarded as the
 %           desired number of q-bins.
+%        samplenames: the names of the samples to re-integrate. Either one
+%           string or a cell array. Supply an empty cell {} to re-integrate
+%           every sample.
 %
-% Outputs:
-%        qs: the q vectors in a matrix
-%        ints: the intensities
-%        errs: the errors
-%        areas: the effective areas of q-bins in pixels.
-%        params: the parameters from the logfile.
-% 
-% This script also writes qs, ints, errs to intbinned*.dat files.
+%
+% Notes:
+%        1. This script also writes qs, ints, errs to intbinned*.dat files.
+%        2. Automatic guessing of the q-range is done this way:
+%              a) determine the q-range for each energy by taking the two
+%                 unmasked pixels which are the nearest and the most far
+%                 from the beam.
+%              b) the automatic q-range will be the intersection of all
+%                 these.
+%  
 %
 % Created: 27.4.2009 Andras Wacha
 % Edited: 20.5.2009 AW. save data to intbinned*.dat, do not overwrite intnorm*.dat
@@ -30,92 +40,138 @@ function [qs,ints,errs,areas,params]=reintegrateB1pilatus(fsn,mask,sddistance,qr
 % Edited: 27.5.2009 AW the q-range is generated from the mask. Also the
 % meaning of qrange has changed.
 % Edited: 5.6.2009 AW now radint is called with 1-MASK and NOT with MASK
+% Edited: 24.5.2009 AW guessing the common q-range is done by taking the
+%     mask into account. Giving a scalar value for the q-range now should
+%     work.
+% Edited: 27.9.2009 AW now it is capable to handle multiple samples
+%     measured at multiple distances. The return values are removed!
+% Edited: 29.10.2009 UV: first q-value is not saved anymore, because it is
+%     not correct anyway
 
 hc = 2*pi*1973.269601; % from qfrompixelsizeB1
 
 fsn=fsn(:);
-disp('Loading header files...');
-counter=0;
-for i=1:length(fsn)
-    tmp=readlogfilepilatus(sprintf('intnorm%d.log',fsn(i)));
-    if isstruct(tmp)
-        if(tmp.Dist == sddistance) % Added restriction that only one distance is loaded, UV 16.7.2009
-           counter=counter+1;
-           params(counter)=tmp;
-        end;
-    end
+
+if nargin<3 % if sddistance was not supplied, set it to [], which activates
+            % the "autodetect" mechanism.
+    sddistance=[];
 end
-disp('...done.');
 
 if nargin<4 %qrange was not supplied. 3 before, 4 after adding sddistance UV
     disp('Determining common q-range for given FSN range.');
     Nq=sqrt(size(mask,2)^2+size(mask,1)^2); % default number of auto-generated q-bins
     qrange=[]; %empty to signal that this should be generated.
 elseif numel(qrange)==1 % if qrange was supplied and is a scalar
-    qrange=[]; % empty to signal that this should be generated.
+    Nq=qrange; % regard it as the preferred number of q-bins.
+    qrange=[]; % empty it to signal that this should be generated.
+else
+    %qrange is given. Nq is its length\
+    Nq=numel(qrange);
+    qrange=qrange(:); %vectorize it. Who knows...
 end;
 
-if isempty(qrange) % qrange is to be generated
-    Nq=ceil(Nq); %who knows...
-    refbefore=strcmp({params.Title},'Reference_on_GC_holder_before_sample_sequence');
-    refafter=strcmp({params.Title},'Reference_on_GC_holder_after_sample_sequence');
-    notreference=~refbefore & ~refafter; % this logical matrix can be used to index
-                                         % params. Thus:
-                                         % params(notreference) will be the
-                                         % non-reference subset of params() 
-    dists=[params(notreference).Dist]; % decide if we have measurements from multiple geometries
-    dist=unique(dists);
-    if length(dist)>1
-        disp('STOPPING. Measurements with more distances are to be re-integrated.\nPlease give only one.');
-        return;
-    end;
-    energymin=min([params.EnergyCalibrated]);
-    energymax=max([params.EnergyCalibrated]);
-    % Create the D-matrix, which contains the distances of the pixels from
-    % the beam center.
-    x=params(1).PixelSize*[1:size(mask,1)].'*ones(1,size(mask,2));
-    y=params(1).PixelSize*ones(size(mask,1),1)*[1:size(mask,2)];
-    D=sqrt((x-params(1).PixelSize*params(1).BeamPosX).^2+(y-params(1).PixelSize*params(1).BeamPosY).^2);
-    D=D(:); % vectorize it so we can use one min() and one max() function
-    rmin=min(D);
-    rmax=max(D);
-    qmin=4*pi*sin(0.5*atan(rmin/dist))*energymax/hc;
-    qmax=4*pi*sin(0.5*atan(rmax/dist))*energymin/hc;
-    qrange=linspace(qmin,qmax,Nq);
-    disp(sprintf('Created q-range. Q_min=%f, q_max=%f, q_step=%f, Nq=%d',qmin,qmax,qrange(2)-qrange(1),Nq));
-end
-%now we have the q-range in qrange.
-qrange=qrange(:); %vectorize it
-qs=zeros(length(qrange),counter); %we create the empty matrices
-ints=qs;
-errs=qs;
-areas=qs;
-for i = 1:counter;
-    disp(sprintf('Loading 2d data for FSN %d',params(i).FSN));
-    [As,Aerrs]=read2dintfilepilatus(params(i).FSN);
-    disp(sprintf('Re-integrating FSN %d (%s)',params(i).FSN,params(i).Title));
-   [q1,I1,err1,area1]=radint(As,...
-                             Aerrs,...
-                             params(i).EnergyCalibrated,...
-                             params(i).Dist,...
-                             params(i).PixelSize,...
-                             params(i).BeamPosX,...
-                             params(i).BeamPosY,...
-                             1-mask,qrange);
-   qs(:,i)=q1(:);
-   ints(:,i)=I1(:);
-   errs(:,i)=err1(:);
-   areas(:,i)=area1(:);
-   name = sprintf('intbinned%d.dat',params(i).FSN);
-   fid = fopen(name,'w');
-   if(fid > -1)
-         for(m = 1:size(qs,1))
-           fprintf(fid,'%e %e %e\n',qs(m,i),ints(m,i),errs(m,i));
-         end;
-         fclose(fid);
-         disp(sprintf('Saved data to file %s',name));
-      else
-         disp(sprintf('Unable to save data to file %s',name));
-   end;
 
+if nargin<5 % if sample names were not supplied, set the list to an empty
+            % cell array, which corresponds to "autodetection".
+    samplenames={};
+end;
+
+if ~iscell(samplenames) % if only one was supplied, make a list of length=1
+    samplenames={samplenames};
 end
+
+
+% Load all header files.
+disp('Loading header files...');
+counter=0;
+for i=1:length(fsn)
+    tmp=readlogfilepilatus(sprintf('intnorm%d.log',fsn(i)));
+    if isstruct(tmp) % if loading was successful
+% removed this check. This is now done at a different part of this script,
+% see below. AW. 27.09.2009.
+%        if(tmp.Dist == sddistance || sddistance<=0) % Added restriction that only one distance is loaded, UV 16.7.2009.
+%                                                    % Added overriding default value sddistance<=0. Now one can omit sddistance. AW 27.9.2009.
+           counter=counter+1;
+           params(counter)=tmp;
+%        end;
+    end
+end
+disp('...done.');
+
+% autodetect samplenames, if needed.
+if isempty(samplenames)
+    samplenames=unique({params.Title}); % find all the samplenames and treat them one-by-one
+end
+
+for si=1:length(samplenames) % treat each sample one-by-one
+   disp(sprintf('Re-integrating sample %s\n',samplenames{si}))
+   % select the parameters corresponding only to this sample. I prefix them
+   % with 's'.
+   sparams=params(strcmp({params.Title},samplenames{si}));
+   
+   % if needed, autodetect sample-to-detector distances. Note, that we do
+   % not overwrite sddistance, because we will need its default value in
+   % the next iteration of this loop.
+   if isempty(sddistance) % no sample-to-detector distance was given
+      sdist=unique([sparams.Dist]); % find all distances
+   else
+      sdist=sddistance; % fetch value to sdist from sddistance.
+   end
+   % now we have sdist as a vector.
+   for dist=sdist %treat each distance separately
+       disp(sprintf('Re-integrating measurements at %f distance for sample %s\n',dist,samplenames{si}));
+       
+       %select the param structures for this sample with this distance.
+       sdparams=sparams([sparams.Dist]==dist);
+       
+       % if q-range is needed to be generated, do so.
+       if isempty(qrange) % qrange is to be generated
+           Nq=ceil(Nq); %who knows...
+           energymin=min([sdparams.EnergyCalibrated]);
+           energymax=max([sdparams.EnergyCalibrated]);
+           % Create the D-matrix, which contains the distances of the pixels from
+           % the beam center.
+           x=sdparams(1).PixelSize*[1:size(mask,1)].'*ones(1,size(mask,2));
+           y=sdparams(1).PixelSize*ones(size(mask,1),1)*[1:size(mask,2)];
+           D=sqrt((x-sdparams(1).PixelSize*sdparams(1).BeamPosX).^2+(y-sdparams(1).PixelSize*sdparams(1).BeamPosY).^2);
+           D=D(mask~=0); % select only the non-masked points
+           rmin=min(D);
+           rmax=max(D);
+           qmin=4*pi*sin(0.5*atan(rmin/dist))*energymax/hc;
+           qmax=4*pi*sin(0.5*atan(rmax/dist))*energymin/hc;
+           sdqrange=linspace(qmin,qmax,Nq);
+           disp(sprintf('Created q-range. Q_min=%f, q_max=%f, q_step=%f, Nq=%d',qmin,qmax,sdqrange(2)-sdqrange(1),Nq));
+       else
+           sdqrange=qrange; % take a copy of it.
+       end
+       %now we have the q-range in qrange.
+       sdqrange=sdqrange(:); %vectorize it
+       % now iterate through 
+       for i = 1:numel(sdparams);
+           disp(sprintf('Loading 2d data for FSN %d',sdparams(i).FSN));
+           [As,Aerrs]=read2dintfilepilatus(sdparams(i).FSN);
+           disp(sprintf('Re-integrating FSN %d (%s)',sdparams(i).FSN,sdparams(i).Title));
+           [q1,I1,err1,area1]=radint(As,...
+                                     Aerrs,...
+                                     sdparams(i).EnergyCalibrated,...
+                                     sdparams(i).Dist,...
+                                     sdparams(i).PixelSize,...
+                                     sdparams(i).BeamPosX,...
+                                     sdparams(i).BeamPosY,...
+                                     1-mask,sdqrange);
+           name = sprintf('intbinned%d.dat',sdparams(i).FSN);
+           fid = fopen(name,'w');
+           if(fid > -1)
+                 for(m = 2:length(q1)) % Don't save the first value because it's wrong, UV 29.10.2009
+                   fprintf(fid,'%e %e %e\n',q1(m),I1(m),err1(m));
+                 end;
+                 fclose(fid);
+                 disp(sprintf('Saved data to file %s',name));
+           else
+                 disp(sprintf('Unable to save data to file %s',name));
+           end;
+        
+       end % i=1:numel(sdparams)
+   end % for dist...
+end % for samplename
+
